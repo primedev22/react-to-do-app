811d2f9515dbdbdffb25e6a6e8fe55ecb08029cd
Prime Dev <prime.dev22@gmail.com>
2020-11-15 02:33:06 +0900

    Implement CRUD APIs - todos, add, update, remove

--- server/handler.ts
+++ server/handler.ts
@@ -48,8 +48,108 @@ export const todos = async (_event, _context) => {
   }
 };
 
-export const add = async (_event, _context) => {};
+export const add = async (_event, _context) => {
+  const data = JSON.parse(_event.body);
+  const params = {
+    TableName: "todosTable",
+    Item: {
+      id: { S: uuid() },
+      title: { S: data.title },
+      description: { S: data.description },
+      done: { BOOL: false },
+      createdAt: { S: new Date().toISOString() },
+    },
+  };
+  try {
+    await dynamodb.putItem(params).promise();
 
-export const update = async (_event, _context) => {};
+    const {
+      id: { S: id },
+      title: { S: title },
+      description: { S: description },
+      done: { BOOL: done },
+      createdAt: { S: createdAt },
+    } = params.Item;
 
-export const remove = async (_event, _context) => {};
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true,
+        item: { id, title, description, done, createdAt },
+      }),
+    };
+  } catch (e) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: e.message }, null, 2),
+    };
+  }
+};
+
+export const update = async (_event, _context) => {
+  const data = JSON.parse(_event.body);
+  const params = {
+    TableName: "todosTable",
+    Key: {
+      id: { S: data.id },
+    },
+    ExpressionAttributeNames: {
+      "#done": "done",
+    },
+    ExpressionAttributeValues: {
+      ":done": { BOOL: data.done },
+    },
+    UpdateExpression: "SET #done = :done",
+    ReturnValues: "ALL_NEW",
+  };
+  try {
+    const data = await dynamodb.updateItem(params).promise();
+
+    const {
+      id: { S: id },
+      title: { S: title },
+      description: { S: description },
+      done: { BOOL: done },
+      createdAt: { S: createdAt },
+    } = data.Attributes;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true,
+        item: { id, title, description, done, createdAt },
+      }),
+    };
+  } catch (e) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: e.message }, null, 2),
+    };
+  }
+};
+
+export const remove = async (_event, _context) => {
+  const data = JSON.parse(_event.body);
+  var params = {
+    TableName: "todosTable",
+    Key: {
+      id: { S: data.id },
+    },
+  };
+  try {
+    await dynamodb.deleteItem(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true,
+        id: data.id,
+      }),
+    };
+  } catch (e) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: e.message }, null, 2),
+    };
+  }
+};
